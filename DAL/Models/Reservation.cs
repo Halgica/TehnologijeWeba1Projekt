d8ed0cf1d850faff96fe5212c142c438faad8740
Halgica <161626237+Halgica@users.noreply.github.com>
2025-05-06 20:57:37 +0200

    lude promjene

---
+++ DAL/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public virtual User? Rezervator { get; set; }
+    }
+}
