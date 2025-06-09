b5716d434a79606c248d53450653b3c8fb09d5bb
Halgica <161626237+Halgica@users.noreply.github.com>
2025-06-09 21:19:53 +0200

    Tests fix

--- ReservationAPI/Program.cs
+++ ReservationAPI/Program.cs
@@ -1,123 +1,226 @@
-using DAL.DB;
-using DAL.Repos;
-using ReservationAPI.DTOs;
-using DAL.Models;
-using System.Text;
-using Serilog;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using ReservationAPI.Settings;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using ReservationAPI.Middleware;
-using ReservationAPI.Services;
-using Microsoft.AspNetCore.Identity;
-using DAL.Repos.Interface;
+//using DAL.DB;
+//using DAL.Repos;
+//using ReservationAPI.DTOs;
+//using DAL.Models;
+//using System.Text;
+//using Serilog;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.IdentityModel.Tokens;
+//using ReservationAPI.Settings;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using ReservationAPI.Middleware;
+//using ReservationAPI.Services;
+//using Microsoft.AspNetCore.Identity;
+//using DAL.Repos.Interface;
 
-var builder = WebApplication.CreateBuilder(args);
+//var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog((context, services, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration));
+//builder.Host.UseSerilog((context, services, configuration) =>
+//    configuration.ReadFrom.Configuration(context.Configuration));
 
-builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+//builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings.Issuer,
-            ValidAudience = jwtSettings.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
-        };
-    });
-
-//// In your Startup.cs or Program.cs
-//builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-//    .AddEntityFrameworkStores<TW1DbContext>();
-
-builder.Services.AddAutoMapper(typeof(Program));
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
-builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
-builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
-builder.Services.AddScoped<IPromotionRepository, PromotionRepository>();
-builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
-builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-builder.Services.AddScoped<ITimeSlotRepository, TimeSlotRepository>();
-
-builder.Services.AddScoped<AuthService>();
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy.WithOrigins("http://localhost:5178")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
-});
-
-
-
-builder.Services.AddDbContext<TW1DbContext>(options =>
-{
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        x => x.MigrationsAssembly("DAL")
-    )
-    .UseLazyLoadingProxies()
-    .LogTo(Console.WriteLine, LogLevel.Information);
-});
-
-var app = builder.Build();
-
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-// Automatic database migration upon startup
-//using (var scope = app.Services.CreateScope())
+//builder.Services.AddAuthentication(options =>
 //{
-//    var db = scope.ServiceProvider.GetRequiredService<TW1DbContext>();
-//    db.Database.Migrate();
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//    .AddJwtBearer(options =>
+//    {
+//        var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuer = true,
+//            ValidateAudience = true,
+//            ValidateLifetime = true,
+//            ValidateIssuerSigningKey = true,
+//            ValidIssuer = jwtSettings.Issuer,
+//            ValidAudience = jwtSettings.Audience,
+//            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+//        };
+//    });
+
+////// In your Startup.cs or Program.cs
+////builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+////    .AddEntityFrameworkStores<TW1DbContext>();
+
+//builder.Services.AddAutoMapper(typeof(Program));
+
+//// Add services to the container.
+//builder.Services.AddControllers();
+
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+//builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
+//builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
+//builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+//builder.Services.AddScoped<IPromotionRepository, PromotionRepository>();
+//builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+//builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+//builder.Services.AddScoped<ITimeSlotRepository, TimeSlotRepository>();
+
+//builder.Services.AddScoped<AuthService>();
+
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowFrontend", policy =>
+//    {
+//        policy.WithOrigins("http://localhost:5178")
+//              .AllowAnyHeader()
+//              .AllowAnyMethod()
+//              .AllowCredentials();
+//    });
+//});
+
+
+
+//builder.Services.AddDbContext<TW1DbContext>(options =>
+//{
+//    options.UseSqlServer(
+//        builder.Configuration.GetConnectionString("DefaultConnection"),
+//        x => x.MigrationsAssembly("DAL")
+//    )
+//    .UseLazyLoadingProxies()
+//    .LogTo(Console.WriteLine, LogLevel.Information);
+//});
+
+//var app = builder.Build();
+
+//app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
 //}
 
-app.UseHttpsRedirection();
+//// Automatic database migration upon startup
+////using (var scope = app.Services.CreateScope())
+////{
+////    var db = scope.ServiceProvider.GetRequiredService<TW1DbContext>();
+////    db.Database.Migrate();
+////}
 
-app.UseRouting();
+//app.UseHttpsRedirection();
 
-app.UseCors("AllowFrontend");
+//app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
+//app.UseCors("AllowFrontend");
 
-app.UseStaticFiles();
+//app.UseAuthentication();
+//app.UseAuthorization();
 
-app.MapControllers();
+//app.UseStaticFiles();
+
+//app.MapControllers();
 
 
-app.Run();
+//app.Run();
+
+using DAL.DB;
+using DAL.Repos.Interface;
+using DAL.Repos;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ReservationAPI.Middleware;
+using ReservationAPI.Services;
+using ReservationAPI.Settings;
+using Serilog;
+using System.Text;
+
+public class Program // Iz nekog razloga mi testovi ne rade ako nema klase program ???
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Host.UseSerilog((context, services, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration));
+
+        builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+            };
+        });
+
+        builder.Services.AddAutoMapper(typeof(Program));
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
+        builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
+        builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+        builder.Services.AddScoped<IPromotionRepository, PromotionRepository>();
+        builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+        builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+        builder.Services.AddScoped<ITimeSlotRepository, TimeSlotRepository>();
+        builder.Services.AddScoped<AuthService>();
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowFrontend", policy =>
+            {
+                policy.WithOrigins("http://localhost:5178")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowCredentials();
+            });
+        });
+
+        builder.Services.AddDbContext<TW1DbContext>(options =>
+        {
+            options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection"),
+                x => x.MigrationsAssembly("DAL")
+            )
+            .UseLazyLoadingProxies()
+            .LogTo(Console.WriteLine, LogLevel.Information);
+        });
+
+        var app = builder.Build();
+
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseCors("AllowFrontend");
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseStaticFiles();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
+
